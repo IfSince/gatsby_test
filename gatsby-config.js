0f11ec25f16f20eb6aee57efc62a9ea4edce8411
IfSince <leonlaade@gmx.de>
2023-09-23 22:40:57 +0200

    add tailwind setup and remove other css

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${ __dirname }/src/images`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -39,5 +39,6 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    'gatsby-plugin-postcss',
   ],
 }
