bc3b5f9e8d46727f1d71d4ce5b5a8a0810711e4e
IfSince <leonlaade@gmx.de>
2023-09-24 16:17:56 +0200

    remove braces

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,9 +3,7 @@ import { useEffect } from 'react'
 import Seo from '../components/seo'
 import { navigate } from 'gatsby';
 
-const NotFoundPage = () => {
-  useEffect(() => navigate('/'), []);
-}
+const NotFoundPage = () => useEffect(() => navigate('/'), [])
 
 export const Head = () => <Seo title="404: Not Found"/>
 
